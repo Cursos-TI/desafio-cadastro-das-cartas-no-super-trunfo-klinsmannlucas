40416019be191abc8d60e9b907be7afdd1c18bbd
klinsmann lucas <multiklinsmann@gmail.com>
2025-07-03 09:54:17 -0300

    Nível Mestre: Comparação e Super Poder, adicionei Super Poder, comparei as duas cartas, e modifiquei o tipo de varavel de alguns atributos para que não ocorra problemas de falta de memória.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,7 +4,7 @@
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+
 
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
@@ -13,18 +13,18 @@ int main() {
     // Variáveis para carta 1:
     char nomeDoEstado1;          //Estado de A a H
     char codigoDoEstado1[4];     //Código do estado, ex A01
-    int populacao1;
+    unsigned long int populacao1;
     int quantidadeDePontosTuristicos1;
     float area1;
-    float pib1;
-
+    double pib1;
+    
     // Variáveis para carta 2:
     char nomeDoEstado2;          //Estado de A a H
     char codigoDoEstado2[4];     //Código do estado, ex A01
-    int populacao2;
+    unsigned long int populacao2;
     int quantidadeDePontosTuristicos2;
     float area2;
-    float pib2;
+    double pib2;
 
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
@@ -38,7 +38,7 @@ int main() {
     nomeDoEstado1 = codigoDoEstado1[0];
 
     printf("Digite a população do estado da carta 1: \n");
-    scanf("%d", &populacao1);
+    scanf("%lu", &populacao1);
 
     printf("Digite a quantidade de pontos turísticos no estado da carta 1: \n");
     scanf("%d", &quantidadeDePontosTuristicos1);
@@ -47,7 +47,7 @@ int main() {
     scanf("%f", &area1);
 
     printf("Digite o PIB do estado da carta 1: \n");
-    scanf("%f", &pib1);
+    scanf("%lf", &pib1);
 
     //Coleta de dados para a carta do estado 2.
     
@@ -58,7 +58,7 @@ int main() {
     nomeDoEstado2 = codigoDoEstado2[0];
 
     printf("Digite a população do estado da carta 2: \n");
-    scanf("%d", &populacao2);
+    scanf("%lu", &populacao2);
 
     printf("Digite a quantidade de pontos turísticos no estado da carta 2: \n");
     scanf("%d", &quantidadeDePontosTuristicos2);
@@ -67,7 +67,7 @@ int main() {
     scanf("%f", &area2);
 
     printf("Digite o PIB do estado da carta 2: \n");
-    scanf("%f", &pib2);
+    scanf("%lf", &pib2);
     
     
     // Exibição dos Dados das Cartas:
@@ -79,24 +79,49 @@ int main() {
     printf("Carta 1: \n");
     printf("Nome do estado: %c \n", nomeDoEstado1);
     printf("Códico da carta 1: %s \n", codigoDoEstado1);
-    printf("População : %d \n",populacao1);
+    printf("População : %lu \n",populacao1);
     printf("Quantidade de pontos túriscos: %d \n",quantidadeDePontosTuristicos1);
     printf("Área: %.2f \n",area1);
-    printf("PIB: %.2f \n",pib1);
-    printf("Densidade Populacional: %.2f \n", (populacao1/area1));
-    printf("PIB per Capita: %.2f \n", (pib1/populacao1));
+    printf("PIB: %.2lf \n",pib1);
+
+    double densidadePopulacional1 = (double)populacao1/area1;
+    double pibPerCapita1 = pib1/area1;
+    // Casting do unsigned long int para double, para evitar a perda de possivel dados caso for convertido pra float, o restanto é mantido em float.
+    double superPoder1 = ((double)populacao1 + area1 + pib1 + (float)quantidadeDePontosTuristicos1 + pibPerCapita1)/densidadePopulacional1;
+
+    printf("Densidade Populacional: %.2lf \n", densidadePopulacional1);
+    printf("PIB per Capita: %.2f \n", pibPerCapita1);
+    printf("Super Poder: %.2lf \n", superPoder1);
 
     // Exibindo os dados da carta 2:
     printf("\n");
     printf("Carta 2: \n");
     printf("Nome do estado: %c \n", nomeDoEstado2);
     printf("Códico da carta 2: %s \n", codigoDoEstado2);
-    printf("População : %d \n",populacao2);
+    printf("População : %lu \n", populacao2);
     printf("Quantidade de pontos túriscos: %d \n",quantidadeDePontosTuristicos2);
     printf("Área: %.2f \n",area2);
-    printf("PIB: %.2f \n",pib2);
-    printf("Densidade Populacional: %.2f \n", (populacao2/area2));
-    printf("PIB per Capita: %.2f \n", (pib2/populacao2));
+    printf("PIB: %.2lf \n",pib2);
+
+    double densidadePopulacional2 = (double)populacao2/area2;
+    double pibPerCapita2 = pib2/area2;
+    // Casting do unsigned long int para double, para evitar a perda de possivel dados caso for convertido pra float, o restanto é mantido em float.
+    double superPoder2 = ((double)populacao2 + area2 + pib2 + quantidadeDePontosTuristicos2 + pibPerCapita2)/densidadePopulacional2;
+
+    printf("Densidade Populacional: %.2lf \n", densidadePopulacional2);
+    printf("PIB per Capita: %.2f \n", pibPerCapita2);
+    printf("Super Poder: %.2lf \n", superPoder2);
+
+    // Comparação das cartas
+    printf("\n");
+    printf("Comparacão de Cartas: \n");
+    printf("População: %d\n", populacao1 > populacao2);
+    printf("Área: %d\n", area1 > area2);
+    printf("PIB: %d\n", pib1 > pib2);
+    printf("Pontos Turísticos: %d\n", quantidadeDePontosTuristicos1 > quantidadeDePontosTuristicos2);
+    printf("Densidade Populacional: %d\n", densidadePopulacional1 < densidadePopulacional2);
+    printf("PIB per Capita: %d\n", pibPerCapita1 > pibPerCapita2);
+    printf("Super Poder: %d\n", superPoder1 > superPoder2);
 
     return 0;
 }
